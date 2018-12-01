a25177c4205028c43bf2755ae7cacae056b72331
Soloi <42078841+Soloi@users.noreply.github.com>
2018-12-01 12:48:37 +0100

    autentiserar genom att jämföra inskrivet password med password i angivet Member-objekt

--- src/authenticate.java
+++ src/authenticate.java
@@ -1,3 +1,27 @@
-public class authenticate{
-  
+/*tar in 'användarnamnnamn' och 'lösenord' som parametrar, och returnar
+en boolean. Villkoret för true är att användarnamn equals ett namnet på ett
+ skapat member-objekt, och att lösenord matchar 'password', som är en variabel
+ i samma member-objekt.
+*/
+
+public class authenticate {
+
+    private String username;
+    private String password;
+    private boolean result;
+
+    public boolean checkUsernamePassword(String username, String password){
+
+        this.username = username;
+        this.password = password;
+
+        if (this.password equals this.username.getPassword() ) {
+            result = true;
+        }
+        else {
+            result = false;
+        }
+
+        return result;
+    }
 }
